58b1a6d6e71984f6ff583c77a64bcb0ee87402c1
Alex-git-code <alexlaurmircea@gmail.com>
2020-09-04 12:39:59 +0300

    PlayerStats class commiy

---
+++ app/src/main/java/org/alexcode/crazysnakes/PlayerStats.java
@@ -0,0 +1,14 @@
+package org.alexcode.crazysnakes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayerStats extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_stats);
+    }
+}
